f6d424b25807ba770a680ffbc1427ab52bea2a98
Adel Mahmoud <adelmahmoud1337@gmail.com>
2024-07-24 19:27:51 +0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let name = "Adel Mahmoud"
+
+console.log(name.indexOf(" "))
+console.log(name.slice(0,4))
+
+
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const countLabel = document.getElementById("countLabel");
+let num = 0;
+
+decreaseBtn.onclick = function(){
+    num -= 1;
+    countLabel.textContent = num;
+}
+
+increaseBtn.onclick = function(){
+    num += 1;
+    countLabel.textContent = num;
+}
+
+resetBtn.onclick = function(){
+    num = 0;
+    countLabel.textContent = num;
+}
